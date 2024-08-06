6f0f039a863050d8b0312cd9507f2b0376aa7f3c
Lewis-Aucott <43778882+Lewis-Aucott@users.noreply.github.com>
2024-08-06 09:30:34 +0000

    Some kaggle stuff

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 #Import required packages
 import numpy as np
+import pandas as pd
 import math
 import torch
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
-import pandas as pd
+
 
 
 #Read in data source
-training_data = np.loadtxt('data/train.csv',delimiter=",",dtype=np.float32,skiprows=1)
-train_x=torch.from_numpy(xy[:, 1:])
-train_y=torch.from_numpy(xy[:, [0]])
-n_samples=xy.shape[0]
+df = pd.read_csv('data/train.csv')
 
-print(train_x)
-# train_set=DataLoader(training_data, batch_size=64, shuffle=True)
+# Drop some columns and separarate into x/y values
+train_y=df.pop('Exited')
+train_X=df.drop(['id','CustomerId','Surname','Geography'],axis=1)
+print(train_X.columns)
+print(df)
+
+# Encoding and scaling
+
+def genderEncoding(gender):
+  if (gender=='male'):
+    return 1
+  else:
+    return 0
     
+train_X.Gender=train_X.Gender.apply(genderEncoding)
+train_X.Gender.apply(genderEncoding)
+print(train_X.Gender)
+
+
+
+
+
+
+# Wrap iterable to iterate over epoch
+
+# train_set=DataLoader(training_data, batch_size=64, shuffle=True)
+
 # train_features=next(iter(train_set))
 
 # print(train_features)
